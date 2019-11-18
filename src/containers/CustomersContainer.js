2d4ae95c5bc577e33e875d9b44d7bf9d08eeb06e
hbened01 <humbertobenedito@gmail.com>
2019-11-18 13:53:30 -0300

    update proyect customers

--- src/containers/CustomersContainer.js
+++ src/containers/CustomersContainer.js
@@ -4,32 +4,14 @@ import AppFrame from './../components/AppFrame';
 import CustomersList from './../components/CustomersList';
 import CustomersActions from './../components/CustomersActions';
 import { withRouter } from 'react-router-dom';
-
-const customers = [
-    {
-        'dni': '2399009748',
-        'name': 'Pepe Perez',
-        'age': 34,
-    },
-    {
-        'dni': '2390078000',
-        'name': 'Marco Perez',
-        'age': 33,
-    },
-    {
-        'dni': '2399976908',
-        'name': 'Jesus Colmenares',
-        'age': 31,
-    },
-    {
-        'dni': '2399008598',
-        'name': 'Santos',
-        'age': 26,
-    },
-];
-
+import { connect } from 'react-redux';
+import { fetchCustomers } from '../actions/fetchCustomers';
 
 class CustomersContainer extends Component {
+
+    componentDidMount() {
+        this.props.fetchCustomers()
+    }
 
     handleAddNew = () => {
         console.log('test click 2')
@@ -53,7 +35,7 @@ class CustomersContainer extends Component {
             <div>
                 <AppFrame
                     header={'Listado de clientes'}
-                    body={this.renderBody(customers)}
+                    body={this.renderBody(this.props.customers)}
                 ></AppFrame>
             </div>
         );
@@ -61,7 +43,33 @@ class CustomersContainer extends Component {
 }
 
 CustomersContainer.propTypes = {
-
+    fetchCustomers: PropTypes.func.isRequired,
+    customers: PropTypes.array.isRequired,
 };
 
-export default withRouter(CustomersContainer);
+CustomersContainer.defaultProps = {
+    customers: [
+        {
+            'dni': '2399009748',
+            'name': 'Pepe Perez',
+            'age': 34,
+        },
+        {
+            'dni': '2390078000',
+            'name': 'Marco Perez',
+            'age': 33,
+        },
+        {
+            'dni': '2399976908',
+            'name': 'Jesus Colmenares',
+            'age': 31,
+        },
+        {
+            'dni': '2399008598',
+            'name': 'Santos',
+            'age': 26,
+        },
+    ]
+}
+
+export default withRouter(connect(null, {fetchCustomers})(CustomersContainer));
